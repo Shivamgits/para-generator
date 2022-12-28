cfa4c42dcfdd3a80b1e2df0321db315588c56050
shivam singh <92594066+Shivamgits@users.noreply.github.com>
2022-12-28 10:11:11 +0530

    1

---
+++ script.js
@@ -0,0 +1,29 @@
+const input = document.getElementById("numOfWords");
+const container = document.querySelector(".container");
+const getWords = (n) => {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let data = "";
+  
+    
+    for(let i =0;i<n;++i){
+        const random = (Math.random() * 25).toFixed(0);
+      data += letters[random];
+    }
+
+    return data;
+
+}
+//console.log(getWords());
+const getData = () => {
+  const  numOfWords = Number(input.value);
+    const para = document.createElement("p");
+    let text = "";
+    for(let i = 0;i< numOfWords;++i){
+        const random1 = (Math.random() * 15).toFixed(0);
+        text += getWords(random1);
+        text+= " ";
+    }
+    para.innerText = text;
+    para.setAttribute("class" , "paras");
+ container.append(para);
+}
